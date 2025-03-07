b3b3b2cf3bc53f765206d322398435c20aa1da39
Jhonatas Santos <grigorio.ssantos@gmail.com>
2025-03-07 12:07:32 -0300

    adjust main.jsx to deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,27 @@
+import React from "react";
 import { createRoot } from 'react-dom/client'
+import { render } from "react-dom";
 import { BrowserRouter } from 'react-router-dom'
 import AppContextProvider from './context/AppContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AppContextProvider> 
-      <App />
-    </AppContextProvider>
-  </BrowserRouter>  
-)
+// createRoot(document.getElementById('root')).render(
+//   <BrowserRouter>
+//     <AppContextProvider> 
+//       <App />
+//     </AppContextProvider>
+//   </BrowserRouter>  
+// )
+
+const container = document.getElementById("root");
+ render(
+  <React.StrictMode>
+    <BrowserRouter>
+//     <AppContextProvider> 
+//       <App />
+//     </AppContextProvider>
+//   </BrowserRouter> 
+ </React.StrictMode>,
+  container
+);
